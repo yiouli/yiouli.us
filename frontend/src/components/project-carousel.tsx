a1fd69e14610b1e0e67080ef5e9e82fea2f0b533
yiouli <yiouli.dev@gmail.com>
2021-10-31 16:14:44 -0700

    first draft of life page

--- frontend/src/components/project-carousel.tsx
+++ frontend/src/components/project-carousel.tsx
@@ -13,13 +13,14 @@ import DataRenderer from './data-renderer';
 import { fetchData } from '../data/utils';
 import React, { useCallback, useEffect } from 'react';
 import { Card, Box } from '@mui/material';
+import Carousel from './carousel';
 
 interface ProjectCardProps {
   projectData: ProjectData,
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = (props) => {
-  const {projectData} = props;
+  const { projectData } = props;
   return <Card elevation={7} sx={{
     // display: 'flex',
     // flexDirection: 'row',
@@ -32,10 +33,10 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
       component='img'
       src={projectData.coverPath}
       alt={projectData.title}
-      sx={{maxWidth: '100%', float: 'left', mr: 3}}
+      sx={{ maxWidth: '100%', float: 'left', mr: 3 }}
     />
-    <Typography variant='h6' sx={{wordWrap: 'break-word', pb: 1}}>{projectData.title}</Typography>
-    <Typography variant='body2' sx={{wordWrap: 'break-word'}}>{projectData.description}</Typography>
+    <Typography variant='h6' sx={{ wordWrap: 'break-word', pb: 1 }}>{projectData.title}</Typography>
+    <Typography variant='body2' sx={{ wordWrap: 'break-word' }}>{projectData.description}</Typography>
   </Card>
 }
 
@@ -46,7 +47,6 @@ export interface ProjectCarouselProps {
 }
 
 const ProjectCarousel: React.FC<ProjectCarouselProps> = (props) => {
-  const [activeStep, setActiveStep] = React.useState(0);
   const [error, setError] = React.useState<AxiosError | null>(null);
   const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
   const [data, setData] = React.useState<ProjectData[]>([]);
@@ -61,52 +61,11 @@ const ProjectCarousel: React.FC<ProjectCarouselProps> = (props) => {
     ))();
   }, []);
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
-
   const getContent = useCallback(() => {
-    return <Paper elevation={0}>
-      <AutoPlaySwipeableViews
-        axis='x'
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-        interval={7000}
-      >
-        {data.map((d, idx) => <ProjectCard projectData={d} key={`project-card-${idx}`} />)}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={data.length}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === data.length - 1}
-          >
-            Next
-            <KeyboardArrowRight />
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            <KeyboardArrowLeft />
-            PREVIOUS
-          </Button>
-        }
-      />
-    </Paper>
-  }, [activeStep, data]);
+    return <Carousel>
+      {data.map((d, idx) => <ProjectCard projectData={d} key={`project-card-${idx}`} />)}
+    </Carousel>
+  }, [data]);
 
   return <DataRenderer isLoaded={isLoaded} error={error} getContent={getContent} />;
 }
